d0ad2ec0cdcd35a5b69d73b8e3677ac48c2c8057
Rémi Bouchar <remi.bouchar@gmail.com>
2013-01-18 22:39:52 +0100

    Refactored it a bit, removed useless part of code and added comment so that it's more readable

--- public/js/isTyping.js
+++ public/js/isTyping.js
@@ -1,66 +1,79 @@
 $(document).ready(function() {
-	isTyping = new IsTyping();
+    isTyping = new IsTyping();
 });
-
+ 
 var IsTyping = function() {
-
-		var TIME_OUT_VALUE = 400; // increase to reduce overhead
+ 
+		var TIME_OUT_VALUE = 500; // increase to reduce overhead
 		isTyping = this;
-		//CHAT_STATUS : "CLEAR","IS_TYPING","DONE_TYPING" 
-		isTyping.chat_status = "CLEAR";
+		//lastActionPerformed : "ON_CLEAR","IS_TYPING","DONE_TYPING", "ON_SUBMIT"
+		isTyping.lastActionPerformed = "ON_CLEAR";
 		isTyping.chat_bar_val = $("#chat-bar").val();
-
-		isTyping.onTyping = function() {}
-		isTyping.onDone = function() {}
-		isTyping.onClearAction = function() {}
-		isTyping.onClear = function() {}
-
+ 
+		isTyping.onTyping = function() {
+			console.log("ON_TYPING")
+		}
+		isTyping.onDone = function() {
+			console.log("ON_DONE")
+		}
+		isTyping.onClearAction = function(message) {
+			console.log("ON_SUBMIT  message: ",message)
+		}
+		isTyping.onClear = function() {
+			console.log("ON_CLEAR")
+		}
+ 
 		$("#chat-bar").keyup(function(e) {
-			var enter_key = false;
-			//console.log(e);
+			//I added this for visibility in the log
+			console.log("---------------");
+ 
 			var message = $("#chat-bar").val();
+			//
+			//this is the ERROR we were having, the whole process was blocked because of a misplace 'if' close
+			// that was englobing the whole function
+			//---> if(isTyping.lastActionPerformed == "IS_TYPING") {
+			setTimeout(function() {
+				var new_message = $("#chat-bar").val();
+				if(isTyping.lastActionPerformed != "DONE_TYPING" && new_message == message && message) {
+					isTyping.lastActionPerformed = "DONE_TYPING"
+					isTyping.onDone();
+					return;
+				}
+				// I moved this block in the timeout instead because what we want to know is if there was a message and it's gone 
+				// i also add a condition on ON_SUBMIT because the event ON_CLEAR was still triggered after the ON_SUBMIT
+				if(!new_message && isTyping.lastActionPerformed != "ON_CLEAR" &&isTyping.lastActionPerformed != "ON_SUBMIT" ) {
+					isTyping.lastActionPerformed = "ON_CLEAR";
+					isTyping.onClear();
+					return
+				}
+			}, TIME_OUT_VALUE);
+ 
 			// 13 : enter
+			var enter_key = false;
 			if(e.keyCode == 13) {
 				if(message != "") {
 					$("#chat-bar").val("");
 					enter_key = true;
 				}
 			}
-			if(e.keyCode == 8 && ! message){
+			// 8 mean backspace
+			if(e.keyCode == 13 && message) {
+				isTyping.lastActionPerformed = "ON_SUBMIT";
+				isTyping.onClearAction(message);
 				return
 			}
-
-			if((isTyping.chat_status == "DONE_TYPING" || isTyping.chat_status == "IS_TYPING") & enter_key) {
-				isTyping.chat_status = "CLEAR";
-				console.log(isTyping.chat_status, " sending ", message);
-				isTyping.onClearAction();
-				return;
-			};
-			if((isTyping.chat_status == "CLEAR" || isTyping.chat_status == "DONE_TYPING") && !enter_key) {
-				isTyping.chat_status = "IS_TYPING";
-				console.log(isTyping.chat_status);
+ 
+			if(message && isTyping.lastActionPerformed != "IS_TYPING") {
+				isTyping.lastActionPerformed = "IS_TYPING"
 				isTyping.onTyping();
-			};
-			if(isTyping.chat_status == "IS_TYPING" ) {
-				var last_val = $("#chat-bar").val();
-				setTimeout(function() {
-					if(isTyping.chat_status == "DONE_TYPING") {
-						return;
-					} else {
-						chat_bar_val = $("#chat-bar").val();
-						if(last_val == chat_bar_val && chat_bar_val) {
-							isTyping.chat_status = "DONE_TYPING";
-							console.log(isTyping.chat_status);
-							isTyping.last_done_event_val = message;
-							isTyping.last_chat_bar_val = chat_bar_val;
-						} else if(!$("#chat-bar").val() && isTyping.chat_status != "CLEAR") {
-							isTyping.chat_status = "CLEAR";
-							isTyping.onClear();
-							console.log(isTyping.chat_status)
-						}
-					}
-				}, TIME_OUT_VALUE);
+				return;
 			}
+ 
+			if(message && (isTyping.lastActionPerformed == "ON_CLEAR" || isTyping.lastActionPerformed == "ON_SUBMIT")) {
+				isTyping.lastActionPerformed = "IS_TYPING"
+				isTyping.onTyping()
+			}
+			//}// the infamous misplaced bracket
 			return true;
 		});
 		return isTyping;
